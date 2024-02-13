9a335b9028015e006fba28183763bf17f5b1c891
isaiaha03 <isaiahalvarez2003@gmail.com>
2024-02-12 19:06:29 -0600

    Finished exercise part one.

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite band?");
+            var band = Console.ReadLine();
+
+            Console.WriteLine($"There once was a kid named {name}, he had a pet {animal} that was {color}, which played in a band called {band}.");
+
         }
+
+        
     }
 }
